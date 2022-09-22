21ce8b36d4cb2a139c36e3f1a1b83345bbb8210d
risingcore <vcjsm2283@gmail.com>
2022-09-22 22:38:13 +0900

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -12,4 +13,12 @@ let port = 4000;
 
 app.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
+
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log('Inserting a new user into the database...');
+      console.log('Loading users from the database...');
+      console.log('Here you can setup and run express / fastify / any other framework.');
+    })
+    .catch((error) => console.log(error));
 });
