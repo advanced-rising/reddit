c787ada1e96de2098f2009db20e4593fb69c5f1b
risingcore <vcjsm2283@gmail.com>
2022-09-23 22:55:59 +0900

    update

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,12 +1,12 @@
 import { IsEmail, Length } from 'class-validator';
-import { Entity, Column, Index, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Index, In, OneToMany, BeforeInsert } from 'typeorm';
 import bcrypt from 'bcryptjs';
 import Post from './Post';
 import Vote from './Vote';
 import BaseEntity from './Entity';
 
 @Entity('users')
-export default class User extends BaseEntity {
+export class User extends BaseEntity {
   @Index()
   @IsEmail(undefined, { message: '이메일 주소가 잘못되었습니다.' })
   @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다.' })
@@ -16,7 +16,7 @@ export default class User extends BaseEntity {
   @Index()
   @Length(3, 32, { message: '사용자 이름은 3자 이상이어야 합니다.' })
   @Column({ unique: true })
-  name: string;
+  username: string;
 
   @Column()
   @Length(6, 255, { message: '비밀번호는 6자리 이상이어야 합니다.' })
