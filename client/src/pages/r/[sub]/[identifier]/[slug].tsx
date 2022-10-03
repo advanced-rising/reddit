26ff92077b502be2d0ca266aec47dbe5e30a7b3b
risingcore <vcjsm2283@gmail.com>
2022-10-03 21:05:38 +0900

    update

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -164,49 +164,51 @@ const PostPage = ({ data }: any) => {
                   )}
                 </div>
                 {/* 댓글 리스트 부분 */}
-                {comments?.map((comment) => (
-                  <div
-                    className='flex justify-start items-start border m-2 rounded-lg'
-                    key={comment.identifier}>
-                    {/* 좋아요 싫어요 기능 부분 */}
-                    <div className='flex-shrink-0 w-10 py-2 text-center rounded-l'>
-                      {/* 좋아요 */}
-                      <div
-                        className='flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'
-                        onClick={() => vote(1, comment)}>
-                        {comment.userVote === 1 ? (
-                          <FaArrowUp className='text-red-500' />
-                        ) : (
-                          <FaArrowUp />
-                        )}
+                <ul className='w-full pb-2 px-2 flex justify-center items-center flex-col gap-2'>
+                  {comments?.map((comment) => (
+                    <li
+                      className='flex justify-start items-start border rounded-lg w-full'
+                      key={comment.identifier}>
+                      {/* 좋아요 싫어요 기능 부분 */}
+                      <div className='flex-shrink-0 w-10 py-2 text-center rounded-l'>
+                        {/* 좋아요 */}
+                        <div
+                          className='flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500'
+                          onClick={() => vote(1, comment)}>
+                          {comment.userVote === 1 ? (
+                            <FaArrowUp className='text-red-500' />
+                          ) : (
+                            <FaArrowUp />
+                          )}
+                        </div>
+                        <p className='text-xs font-bold'>{comment.voteScore}</p>
+                        {/* 싫어요 */}
+                        <div
+                          className='flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500'
+                          onClick={() => vote(-1, comment)}>
+                          {comment.userVote === -1 ? (
+                            <FaArrowDown className='text-red-500' />
+                          ) : (
+                            <FaArrowDown />
+                          )}
+                        </div>
                       </div>
-                      <p className='text-xs font-bold'>{comment.voteScore}</p>
-                      {/* 싫어요 */}
-                      <div
-                        className='flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500'
-                        onClick={() => vote(-1, comment)}>
-                        {comment.userVote === -1 ? (
-                          <FaArrowDown className='text-red-500' />
-                        ) : (
-                          <FaArrowDown />
-                        )}
-                      </div>
-                    </div>
 
-                    <div className='flex flex-col py-2 pr-2'>
-                      <p className='mb-1 text-xs leading-none'>
-                        <Link href={`/u/${comment.username}`}>
-                          <a className='mr-1 font-bold hover:underline'>{comment.username}</a>
-                        </Link>
-                        <span className='text-gray-600'>
-                          {`${comment.voteScore}
+                      <div className='flex flex-col py-2 pr-2'>
+                        <p className='mb-1 text-xs leading-none'>
+                          <Link href={`/u/${comment.username}`}>
+                            <a className='mr-1 font-bold hover:underline'>{comment.username}</a>
+                          </Link>
+                          <span className='text-gray-600'>
+                            {`${comment.voteScore}
                         posts${dayjs(comment.createdAt).format('YYYY-MM-DD HH:mm')}`}
-                        </span>
-                      </p>
-                      <p className=' whitespace-normal break-all	'>{comment.body}</p>
-                    </div>
-                  </div>
-                ))}
+                          </span>
+                        </p>
+                        <p className=' whitespace-normal break-all	'>{comment.body}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </>
             )}
           </div>
